8e994e39eedad3041f9b04a08ca5c58c7f4e00df
Tarnenak Yauheni <tarnenok@gmail.com>
2019-07-16 14:56:06 +0300

    Enable A20 Gate

--- boot/stage2.asm
+++ boot/stage2.asm
@@ -39,12 +39,49 @@ main:
     jmp 0x8:main_stage_3
 
     bits 32
+
 main_stage_3:
+    jmp main_stage3_body
+
+wait_input:
+    in      al, 0x64		; read status register
+    test    al, 2		; test bit 2 (Input buffer status)
+    jnz     wait_input	; jump if its not 0 (not empty) to continue waiting
+    ret
+
+wait_output:
+    in      al, 0x64		; read status register
+    test    al, 1		; test bit 1 (Input buffer status)
+    jz     wait_output	; jump if its 0 (not empty) to continue waiting
+    ret
+
+main_stage3_body:
 	mov		eax, 0x10		; set data segments to data selector (0x10)
 	mov		ds, ax
 	mov		ss, ax
 	mov		es, ax
 	mov		esp, 90000h		; stack begins from 90000h
+
+enable_A20:
+    ; send read output port command
+    mov     al,0xD0
+    out     0x64,al
+    call    wait_output
+    
+    ; read input buffer and store on stack. This is the data read from the output port
+    in      al, 0x60
+    push    eax
+    call    wait_input
+    
+    ; send write output port command
+    mov     al, 0xD1
+    out     0x64, al
+    call    wait_input
+    
+    ; pop the output port data from stack and set bit 1 (A20) to enable
+    pop     eax
+    or      al, 2
+    out     0x60, al
 
     cli
 	hlt ; hault the syst
